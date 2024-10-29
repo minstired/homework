62a26321ff6a3835586cb130d5cf8377de235e80
Ekaterina Shakirova <ekaterinashakirovaa@mail.ru>
2024-10-30 01:31:50 +0500

    feat: add banner layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from './components/Header.vue';
+import Banner from './components/Banner.vue';
 </script>
 
 <template>
 
   <Header />
-  
+  <Banner />
+
 </template>
 
 <style scoped>
---
+++ src/components/Banner.vue
@@ -0,0 +1,64 @@
+<script setup>
+const bannerIMG = './public/bannerSVG.svg'
+</script>
+
+<template>
+    <section id="Banner">
+        <div id="leftBannerSide">
+            <img :src="bannerIMG" alt="Banner Image">
+        </div>
+        <div id="rightBannerSide">
+            <h1 id="accentColor">AI and Machine Learning</h1>
+            <h1>Consulting Services</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A modi nostrum libero at sed saepe laborum quos
+                magni ad? Ducimus perspiciatis asperiores sint eum totam obcaecati. Provident rerum sequi consequuntur.
+            </p>
+        </div>
+
+    </section>
+</template>
+
+<style scoped lang="scss">
+section {
+    height: 30%;
+    display: flex;
+    justify-content: space-evenly;
+    margin: 5% 15%;
+
+    #leftBannerSide {
+        height: 550px;
+        width: 600px;
+        position: relative;
+        overflow: hidden;
+        display: flex;
+
+        img {
+            position: absolute;
+            height: 200%;
+
+            top: -300px;
+            right: -250px;
+        }
+    }
+
+    #rightBannerSide {
+        width: 40%;
+
+        h1 {
+            font-size: 2.5rem;
+            margin-bottom: 0.5rem;
+        }
+
+        p {
+            margin-top: 2.5rem;
+            font-size: 1.5rem;
+            opacity: 0.8;
+        }
+    }
+}
+
+#accentColor {
+    color: #FFC107;
+    font-weight: 800;
+}
+</style>
